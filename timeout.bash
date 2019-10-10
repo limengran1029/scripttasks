4140609a12496c1161d14f92ae4faa3879bdb04e
limengran1029 <mengran.li@yh.nackademin.se>
2019-10-10 12:18:17 -0700

    first commit

---
+++ timeout.bash
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+TIME=$1
+PROCESS=`echo ${@:2}`
+
+$PROCESS &
+PID=$!
+sleep "$TIME"
+
+if kill -0 "$PID";then
+	kill -9 "$PID"
+	echo "TIMEOUT"
+	STATUS=1
+else
+	wait $PID
+	STATUS=$?
+fi 2>/dev/null
+
+echo "SLUTSTATUS: $STATUS "
+
